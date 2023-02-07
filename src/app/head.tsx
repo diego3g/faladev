826cc00bfdd7c02f4ff279fdaa841a9f6acde3a6
Diego Fernandes <diego.schell.f@gmail.com>
2023-02-07 17:31:50 -0300

    chore: Remove default title from head

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,6 @@
 export default function Head() {
   return (
     <>
-      <title>Fala Dev</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="shortcut icon" href="/favicon.png" type="image/png" />
     </>
