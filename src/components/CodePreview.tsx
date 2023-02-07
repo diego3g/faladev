8707962c4d6c5de1381581e06cf7553edd015779
Guilherme Sousa <guilherssousa@gmail.com>
2023-02-07 20:10:17 -0300

    chore: make raw a optional property

--- src/components/CodePreview.tsx
+++ src/components/CodePreview.tsx
@@ -9,13 +9,14 @@ const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"] });
 
 interface CodePreviewProps {
   code: string;
-  raw: string;
+  raw?: string;
 }
 
 export function CodePreview({ code, raw }: CodePreviewProps) {
   const [copy, setCopy] = useState("Copy to Clipboard");
 
   const handleCopyToClipboard = () => {
+    if (!raw) return;
     navigator.clipboard.writeText(raw);
     setCopy("Copied to Clipboard!");
     setTimeout(() => setCopy("Copy to Clipboard"), 2000);
@@ -24,13 +25,15 @@ export function CodePreview({ code, raw }: CodePreviewProps) {
   return (
     <>
       {/*  TODO: Conditionally render Clipboard button to a selected group of files */}
-      <button
-        onClick={handleCopyToClipboard}
-        className="absolute flex items-center right-0 mx-8 gap-x-2 font-semibold z-30 bg-[#2B283B] px-3 py-2 rounded-md text-[#8F8CA8]"
-      >
-        <CopyIcon size={16} />
-        {copy}
-      </button>
+      {raw && (
+        <button
+          onClick={handleCopyToClipboard}
+          className="absolute flex items-center right-0 mx-8 gap-x-2 font-semibold z-30 bg-[#2B283B] px-3 py-2 rounded-md text-[#8F8CA8]"
+        >
+          <CopyIcon size={16} />
+          {copy}
+        </button>
+      )}
       <div
         id="shiki-code"
         style={jetBrainsMono.style}
