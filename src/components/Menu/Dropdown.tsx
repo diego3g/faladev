3413d82940227d5d6b1256f5e47f1b95789d6e43
Zenky <bzenky@gmail.com>
2023-02-15 19:57:42 -0300

    feat: dropdown social icon

--- src/components/Menu/Dropdown.tsx
+++ src/components/Menu/Dropdown.tsx
@@ -1,5 +1,12 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
-import { socialLinks } from './utils/socialLinks'
+import {
+  Github,
+  Instagram,
+  Linkedin,
+  Rocket,
+  Twitter,
+  Youtube
+} from 'lucide-react'
 
 interface DropdownProps {
   children: React.ReactNode
@@ -8,6 +15,39 @@ interface DropdownProps {
 export function Dropdown({
   children
 }: DropdownProps) {
+  const socialLinks = [
+    {
+      name: 'github',
+      url: 'https://github.com/diego3g',
+      icon: <Github size={14} />
+    },
+    {
+      name: 'instagram',
+      url: 'https://instagram.com/dieegosf',
+      icon: <Instagram size={14} />
+    },
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/diego-schell-fernandes/',
+      icon: <Linkedin size={14} />
+    },
+    {
+      name: 'rocketseat',
+      url: 'https://www.rocketseat.com.br/',
+      icon: <Rocket size={14} />
+    },
+    {
+      name: 'twitter',
+      url: 'https://twitter.com/dieegosf',
+      icon: <Twitter size={14} />
+    },
+    {
+      name: 'youtube',
+      url: 'https://www.youtube.com/@rocketseat',
+      icon: <Youtube size={14} />
+    },
+  ]
+
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger asChild>
@@ -15,7 +55,7 @@ export function Dropdown({
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Portal>
-        <DropdownMenu.Content side='right' className="bg-[#2a273f] z-10 flex flex-col gap-2 rounded-lg overflow-auto">
+        <DropdownMenu.Content side='right' className="bg-[#2a273f] z-10 flex flex-col rounded-lg overflow-auto">
           {socialLinks.map(socialItem => {
             return (
               <DropdownMenu.Item key={socialItem.url} className="outline-none">
@@ -23,8 +63,9 @@ export function Dropdown({
                   href={socialItem.url}
                   target="_blank"
                   rel="noreferrer"
-                  className="text-[#E0DEF2] flex text-sm px-4 py-2 hover:bg-[#454066]"
+                  className="text-[#E0DEF2] flex items-center gap-2 text-sm px-4 py-3 hover:bg-[#454066]"
                 >
+                  {socialItem.icon}
                   {socialItem.name}
                 </a>
               </DropdownMenu.Item>
