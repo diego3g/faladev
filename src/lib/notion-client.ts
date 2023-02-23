1ee2d93250b19d5904190ce2fb9d1ad20c6b2732
Gabriel <gabrielvitor.frasao@gmail.com>
2023-02-23 16:22:50 -0300

    refactor: change notion-client to official notion api

--- src/lib/notion-client.ts
+++ src/lib/notion-client.ts
@@ -1,19 +1,24 @@
-import { NotionAPI } from "notion-client";
+import { Client, isFullBlock } from "@notionhq/client"
 
-export const notionClient = new NotionAPI();
+export const notionClient = new Client({ auth: process.env.NOTION_API_KEY })
 
 export async function getCodeBlockFromNotion(pageId: string) {
-  const page = await notionClient.getPage(pageId)
-  const blocks = Object.entries(page.block).map(
-    ([key, value]) => ({ ...value })
-  )
-  const codeBlock = blocks.find(block => block.value.type === "code")
+  const { results } = await notionClient.blocks.children.list({ block_id: pageId })
 
-  if (!codeBlock) {
-    throw new Error("Error when trying to fetch Notion pages")
+  let codeBlock
+
+  for (let block of results) {
+    if (isFullBlock(block) && block.type === "code") {
+      codeBlock = block
+      break
+    }
   }
 
-  const codeBlockContent = codeBlock.value.properties.title[0][0] as string
-  
-  return { content: codeBlockContent }
+  if (!codeBlock) {
+    throw new Error(`Failed to fetch Notion content of ID: ${pageId}`)
+  }
+
+  const { plain_text } = codeBlock.code.rich_text[0]
+
+  return { content: plain_text }
 }
