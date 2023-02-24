48010f50686abc67c87debfa1d14e64b39ebfa78
Gabriel VS Frasão <gabrielvitor.frasao@gmail.com>
2023-02-24 10:08:42 -0300

    refactor: use find method instead of for loop
    Co-authored-by: Diego Fernandes <diego.schell.f@gmail.com>

--- src/lib/notion-client.ts
+++ src/lib/notion-client.ts
@@ -5,14 +5,7 @@ export const notionClient = new Client({ auth: process.env.NOTION_API_KEY })
 export async function getCodeBlockFromNotion(pageId: string) {
   const { results } = await notionClient.blocks.children.list({ block_id: pageId })
 
-  let codeBlock
-
-  for (let block of results) {
-    if (isFullBlock(block) && block.type === "code") {
-      codeBlock = block
-      break
-    }
-  }
+  const codeBlock = results.find(block => isFullBlock(block) && block.type === "code")
 
   if (!codeBlock) {
     throw new Error(`Failed to fetch Notion content of ID: ${pageId}`)
