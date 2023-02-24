04ee81a4135de3ecbd92599768cde5ee1183745b
Gabriel <gabrielvitor.frasao@gmail.com>
2023-02-24 12:45:27 -0300

    fix: return proper type

--- src/lib/notion-client.ts
+++ src/lib/notion-client.ts
@@ -1,11 +1,14 @@
 import { Client, isFullBlock } from "@notionhq/client"
+import { CodeBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints"
 
 export const notionClient = new Client({ auth: process.env.NOTION_API_KEY })
 
 export async function getCodeBlockFromNotion(pageId: string) {
   const { results } = await notionClient.blocks.children.list({ block_id: pageId })
 
-  const codeBlock = results.find(block => isFullBlock(block) && block.type === "code")
+  const codeBlock = results.find(
+    block => isFullBlock(block) && block.type === "code"
+  ) as CodeBlockObjectResponse | undefined
 
   if (!codeBlock) {
     throw new Error(`Failed to fetch Notion content of ID: ${pageId}`)
